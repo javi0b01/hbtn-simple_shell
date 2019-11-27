469972eb50fe53ad4c0772bf552493dc0869c01a
1270holberton <1270@holbertonschool.com>
2019-11-27 16:31:25 -0500

    [Feature] New directory with my personal test.

---
+++ testing/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <dirent.h>
+char **_fgetpath(void);
+char *_fwhich(char **s1, char *s2);
+#endif //MAIN_H
