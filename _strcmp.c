c36ef14ae8d26fb23a2db76d27e6c241511133ce
chmlsud <1188@holbertonschool.com>
2019-11-28 15:00:19 +0000

    Add the comments and fix betty style

--- _strcmp.c
+++ _strcmp.c
@@ -1,5 +1,10 @@
 #include "holberton.h"
-
+/**
+ * _strcmp - Function of the program to compare strings
+ * @s1: The first argument
+ * @s2: The second argument
+ * Return: Exit if is 0
+ */
 int _strcmp(char *s1, char *s2)
 {
 	int var1 = 0;
