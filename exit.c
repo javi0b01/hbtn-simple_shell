b44e854575a10197417c60f0a6a1a39b2bc1e4f4
chmlsud <1188@holbertonschool.com>
2019-11-28 15:03:25 +0000

    Fix the betty style

--- exit.c
+++ exit.c
@@ -1,5 +1,9 @@
 #include "holberton.h"
-
+/**
+ *  exit_func - This function do exit to the program
+ *  @str: This is the string
+ *  Return: Exit to 0
+ */
 int exit_func(char *str)
 {
 	if (_strcmp(str, "exit") == 0)
