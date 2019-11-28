63d23bbabe919242465637c021c4222c885863ac
1270holberton <1270@holbertonschool.com>
2019-11-28 13:39:28 -0500

    update with Betty styles

--- main.h
+++ main.h
@@ -7,7 +7,10 @@
 #include <dirent.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <signal.h>
+#include <errno.h>
+extern char **environ;
 char **_fgetpath(void);
 char *_fwhich(char **s1, char *s2);
-//void ctrlc(int num);
-#endif //MAIN_H
+void _fctrlc(int num);
+#endif
