2e7307a67d4342f715c2e98a18bb7539354011f8
chmlsud <1188@holbertonschool.com>
2019-11-29 00:07:24 +0000

    Fix the comments

--- main.h
+++ main.h
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
 /* Libraries */
 
-=======
-#ifndef MAIN_H
-#define MAIN_H
->>>>>>> 838bfe6ae5ba4617d7fff26fcc0b37b57ac2db87
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -15,7 +10,6 @@
 #include <sys/stat.h>
 #include <signal.h>
 #include <errno.h>
-<<<<<<< HEAD
 #include <dirent.h>
 
 /* Prototypes */
@@ -24,12 +18,7 @@ int _strcmp(char *s1, char *s2);
 int exit_func(char *str);
 char **_fgetpath(void);
 char *_fwhich(char **s1, char *s2);
-void ctrlc(int num);
-#endif /* _MAIN_H_ */
-=======
-extern char **environ;
-char **_fgetpath(void);
-char *_fwhich(char **s1, char *s2);
 void _fctrlc(int num);
-#endif
->>>>>>> 838bfe6ae5ba4617d7fff26fcc0b37b57ac2db87
+extern char **environ;
+#endif /* _MAIN_H_ */
+
